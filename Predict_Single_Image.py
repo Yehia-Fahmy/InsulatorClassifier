fa839f06ca6479210c391635c70c83c68f70a531
Yehia F <yehia.fahmy@uwaterloo.ca>
2021-03-04 12:22:07 -0500

    added some imports

--- Predict_Single_Image.py
+++ Predict_Single_Image.py
@@ -1,3 +1,7 @@
 # this file should be able to predict a single image using the tflite model and print the result to the console
 
-
+import numpy as np
+from tensorflow import keras
+from matplotlib import pyplot as plt
+import time as t
+import pickle
