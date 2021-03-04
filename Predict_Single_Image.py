951c21fa88539890b95447cfd98a466679609770
Yehia F <yehia.fahmy@uwaterloo.ca>
2021-03-04 13:31:31 -0500

    some helpful functions

--- Predict_Single_Image.py
+++ Predict_Single_Image.py
@@ -6,3 +6,26 @@ from matplotlib import pyplot as plt
 import time as t
 import pickle
 import tensorflow as tf
+
+
+# function to convert the time into something readable
+def convert_time(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+
+# function to load .pickle files
+def load_data(file_name):
+    print(f'Loading {file_name}...')
+    file = pickle.load(open(file_name, 'rb'))
+    return file
+
+
+# quick function to show the image
+def show(img):
+    plt.imshow(img, cmap='gray')
+    plt.show()
